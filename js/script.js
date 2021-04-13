cea3cf6ed16afc7cb573b259377d2744d2aea4b5
wilsonconce <wilsonconce18@hotmail.com>
2021-04-13 07:55:52 -0500

    Codigo Comentado

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
+//Llamamos a la variable buscar del archivo html, que su nombre de id es "buscar y limpiar"
 document.querySelector('#buscar').addEventListener('click', function () {
     obtenerDatos();
 });
 document.querySelector('#limpiar').addEventListener('click', function () {
     limpiar();
 });
+
+//Funcion generada para la extracion de datos
 function obtenerDatos() {
-    var cadena = document.getElementById("cadena").value;
-    let url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + cadena;
-    const api = new XMLHttpRequest();
+    var cadena = document.getElementById("cadena").value;    //variable que contiene el valor en la caja de texto para buscar por su nombre
+    let url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + cadena;    //Enlace parta el consuma de la API "The CocktailDB" que su clave gratuita es 1
+    const api = new XMLHttpRequest();//Consulta mediante AJAX (peticiones y uso de respuesta en formato JSON)
     api.open('GET', url, true);
     api.send();
-
-    api.onreadystatechange = function () {
-        if (this.status == 200 && this.readyState == 4) {
-            let datos = JSON.parse(this.responseText);
-            dato = datos.drinks;
-            console.log(dato);
-            var detalles = "";
-            for (var i = 0; i < dato.length; i++) {
+    api.onreadystatechange = function () {//Manejo del evento de llamada
+        
+        if (this.status == 200 && this.readyState == 4) {//Verificamos si la respuesta es valida
+            
+            let datos = JSON.parse(this.responseText);//Convertimos la respuesta en un formato mas comodo
+            dato = datos.drinks;            //simplificamos nuestros resultados y hacerlos mas directos hacia lo que buscamos
+            var detalles = "";            //Variable para almacenar resultados deseados
+            for (var i = 0; i < dato.length; i++) {                //Lenamos la nueva variable
                 detalles += "<tr>" +
                     "<td>" + dato[i].idDrink + "</td>" +
                     "<td>" + dato[i].strCategory + "</td>" +
@@ -26,17 +29,16 @@ function obtenerDatos() {
                     "<td><img wit src=" + dato[i].strDrinkThumb + " width='50' heigt='50'></td>" +
                     "</tr>";
             }
-            document.getElementById("tablaDetalles").innerHTML = detalles;
+            document.getElementById("tablaDetalles").innerHTML = detalles;            //Mandamos a dibujar en nuestra pagina dentro de una tabla
         }
     }
 }
-
-function limpiar() {
+function limpiar() {//Funcion de limpiar los campos del sistema para una nueva busqueda
     var detalles = "";
-    document.getElementById("cadena").value="";
+    document.getElementById("cadena").value = "";
     for (var i = 0; i < dato.length; i++) {
-        detalles += "<tr>" 
-         "</tr>";
+        detalles += "<tr>"
+        "</tr>";
     }
     document.getElementById("tablaDetalles").innerHTML = detalles;
 }
